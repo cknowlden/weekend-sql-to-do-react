82bd003655a73580430ff84fc9e9c1c73254e882
Cali Knowlden <calbearnc@gmail.com>
2024-01-08 21:41:13 -0600

    added useEffect and Axios GET to app.jsx

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,13 +1,44 @@
-import {useState} from 'react';
+import {useState, useEffect} from 'react';
+import './App.css';
+import axios from 'axios';
 
 function App () {
-  
+  const [tasks, setTasks] = useState([]);
+
+  useEffect(() => {
+    //function getTasks() {
+      axios.get('/api/todo').then((response) => {
+        console.log(response);
+        setTasks(response.data);
+      }).catch((error) => {
+        console.log('error in GET', error);
+      });
+    //}
+
+    //getTasks();
+  }, []);
+
   return (
+  <>
     <div>
       <h1>TO DO APP</h1>
+          <p>{tasks.name}</p>
+          <p>{tasks.completed}</p>
     </div>
-  );
+    
 
-}
+  </>
+  );
+};
+
+
+  // <h3>Add New To Do</h3>
+  // {/* <form onSubmit="handleSubmit(event)"> */}
+  //   <label>To Do:</label><input type="text" name="" value="" id="to_do" size="50">
+  //   <br />
+  //   <label>Completed:</label><input type="text" name="" value="" id="completed" size="50">
+  //   <br />
+  //   <button>Submit</button>
+  // {/* </form> */}
 
 export default App
