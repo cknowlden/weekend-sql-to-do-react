a1a7079423f2d695b3474e74d5bc42f7673f63ba
Cali Knowlden <calbearnc@gmail.com>
2024-02-06 19:36:30 -0600

    added dotenv to have running on port 5002

--- src/components/TaskAPI/Task.api.js
+++ src/components/TaskAPI/Task.api.js
@@ -2,15 +2,15 @@ import axios from 'axios';
 
 export const fetchList = () => {
   // axios GET call
-    return axios.get('/api/todo');
+  return axios.get('/api/todo');
 };
 
 export const postTask = (taskData) => {
-    return axios.post('/api/todo', taskData);
+  return axios.post('/api/todo', taskData);
 };
 
 export const updateTaskCompletedStatus = (taskId, taskCompleted) => {
-    return axios.put(`/api/todo/${taskId}`, taskCompleted);
+  return axios.put(`/api/todo/${taskId}`, taskCompleted);
 };
 
 export const deleteTask = (taskId) => {
